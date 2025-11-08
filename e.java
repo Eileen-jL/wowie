37f1da61c4e8e5c76e5ec75bb9ac0528e387faec
Eileen-jL <12381689@c-vusd.org>
2025-11-08 06:52:11 +0000

    Base

--- e.java
+++ e.java
@@ -1,1 +1,17 @@
-
+public class Reverser {
+    // @param word a string
+    //return a string consisting of the characters if word in reverse order
+    public static String reverseword(String word) {
+        String reversed = "";
+        for(int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.substring(i,i + 1);
+        }
+        return reversed;
+    }
+    //@param word a string
+    //@return true if word is a palindrome, false otherwise
+    public static boolean isPalindrome(String word) {
+        String reversed = reverseWord(word);
+        return word.equalsIgnoreCase(reversed);
+    }
+}
